92661e01307dbced92137743f3bf14238884140b
th4ruka <tharukapavith2@gmail.com>
2024-10-21 10:20:35 +0530

    Update CORS.

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://text2picapp.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
